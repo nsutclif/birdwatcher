9136b36725a61c1d895807507fd373b5f17b0a2e
Nathan Sutcliffe <nsutcliffe@i7mini.private>
2016-11-04 19:56:45 -0700

    try to log all sensors

--- source/app.js
+++ source/app.js
@@ -1,25 +1,33 @@
 "use strict";
 
-let Gpio = require('onoff').Gpio;
+// let Gpio = require('onoff').Gpio;
 
-// relays: 17, 27, 22, 23
+// // relays: 17, 27, 22, 23
 
-let relay1 = new Gpio(17, 'out');
+// let relay1 = new Gpio(17, 'out');
 
-relay1.writeSync(0);
+// relay1.writeSync(0);
 
-setTimeout(() => {
-  relay1.writeSync(1);
-}, 500);
+// setTimeout(() => {
+//   relay1.writeSync(1);
+// }, 500);
 
 
 let ds18b20 = require("ds18b20");
 
-ds18b20.sensors((err, ids) => {
-  if (err) {
-    console.log("Error getting sensors: " + err);
-  }
-  else {
-    console.log(JSON.stringify(ids));
-  }
+function sensorsPromise(ds18b20) {
+  return new Promise((resolve, reject) => {
+    ds18b20.sensors((err, ids) => {
+      if (err) {
+        reject(err);
+      } 
+      else {
+        resolve(ids);
+      }
+    });
+  });
+}
+
+sensorsPromise(ds18b20).then((ids) => {
+  console.log(JSON.stringify(ids));
 });
