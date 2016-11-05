205e1b01f2388fd0f773c17da2bcac81aba4bad5
Nathan Sutcliffe <nsutcliffe@i7mini.private>
2016-11-04 20:04:56 -0700

    try to log all temperatures

--- source/app.js
+++ source/app.js
@@ -15,7 +15,7 @@
 
 let ds18b20 = require("ds18b20");
 
-function sensorsPromise(ds18b20) {
+function sensors(ds18b20) {
   return new Promise((resolve, reject) => {
     ds18b20.sensors((err, ids) => {
       if (err) {
@@ -28,6 +28,24 @@ function sensorsPromise(ds18b20) {
   });
 }
 
-sensorsPromise(ds18b20).then((ids) => {
+function temperature(ds18b20, sensor) {
+  return new Promise((resolve, reject) => {
+    ds18b20.temperature(sensor, (err, value) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(value);
+      }
+    })
+  });
+}
+
+sensors(ds18b20).then((ids) => {
   console.log(JSON.stringify(ids));
+  return Promise.all(ids.map(id => {
+    return temperature(ds18b20, id);
+  }));
+}).then((temperatures) => {
+  console.log(JSON.stringify(temperatures));
 });
