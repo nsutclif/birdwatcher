e82b187ff0fdfcd1b5c07c384dc650f335fe33ec
Nathan Sutcliffe <nsutcliffe@i7mini.private>
2016-11-06 06:24:11 -0800

    query sensors in parallel

--- source/app.js
+++ source/app.js
@@ -44,12 +44,12 @@ function temperature(ds18b20, sensor) {
 
 sensors(ds18b20).then((ids) => {
   console.log(JSON.stringify(ids));
-  return ids.reduce((promise, id) => {
-    return promise.then(temperature(ds18b20, id));
-  }, Promise.resolve());
-  // return Promise.all(ids.map(id => {
-  //   return temperature(ds18b20, id);
-  // }));
+  // return ids.reduce((promise, id) => {
+  //   return promise.then(temperature(ds18b20, id));
+  // }, Promise.resolve());
+  return Promise.all(ids.map(id => {
+    return temperature(ds18b20, id);
+  }));
 }).then(() => {
   //console.log(JSON.stringify(temperatures));
 });
